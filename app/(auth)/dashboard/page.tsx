e2ab2b480a4d02aff66341ea6acbb827050c23c0
Shivam Kumar <shivam108ku@gmail.com>
2026-01-30 15:19:05 +0530

    making dashboard

---
+++ app/(auth)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1>Dashboard</h1>
+        <Link href={"/dashboard/reports"}>View Reports</Link>
+    </div>
+  )
+}
+
+export default Dashboard
