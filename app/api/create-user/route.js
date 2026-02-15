05cc37eefe458ef4885cbb1ef0be607b36867c9f
Shivam <shivam108ku@gmail.com>
2026-02-15 17:28:58 +0530

    POST api

--- app/api/create-user/route.js
+++ app/api/create-user/route.js
@@ -1,8 +1,27 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request){
     try {
-        
+        const {name , email , age} = await request.json();
+        if(!name || !email || !age){
+            return NextResponse.json({
+                sucess:false , error:" name , email , age is required"
+            }, {status:400})
+        }
+
+    const emailExists = users.find(user=>user.email === email)
+    if(emailExists){
+        return NextResponse.json({
+            success:false , error:"Exisiting user"
+        },{status:400})
     }
-    catch (err) {
+    // Createing user
+
+    const newUser = {
+        id:users.length + 1,
+    }
+
+    } catch (err) {
 
     }
 }
--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export  const  users = [
+export const users = [
     {
          id:1,
          name:"Shivam Kuamr",
