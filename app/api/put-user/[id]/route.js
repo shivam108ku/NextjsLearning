b4990de8d68e00ceeebdb5292d5ca7134534e30f
Shivam <shivam108ku@gmail.com>
2026-02-16 13:27:33 +0530

    Put func to update some part

--- app/api/put-user/[id]/route.js
+++ app/api/put-user/[id]/route.js
@@ -1,19 +1,51 @@
 import { NextResponse } from "next/server";
 import { users } from "../../hello/route";
 
-export async function PUT(request, { params}){
-    try {
-        const {id} = await params;
-        const userId = parseInt(id);
+export async function PUT(request, { params }) {
+  try {
+    const { id } = await params;
+    const userId = parseInt(id);
 
-        const userIndex = users.findIndex(u=>u.id === userId)
-        if(userIndex === -1){
-            return NextResponse({
-                success:false , error: "User not found"
-            }, {status: 404});
-        }
+    const userIndex = users.findIndex((u) => u.id === userId);
+    if (userIndex === -1) {
+      return NextResponse(
+        {
+          success: false,
+          error: "User not found",
+        },
+        { status: 404 },
+      );
     }
-    catch (err){
 
+    const { name, email, age } = await request.json();
+
+    if (!name || !email || !age) {
+      return NextResponse.json(
+        {
+          sucess: false,
+          error: " name , email , age is required",
+        },
+        { status: 400 },
+      );
     }
+
+    users[userIndex] = {
+        id:userId,
+        name:name,
+        email:email,
+        age:age
+    }
+
+    return NextResponse.json({
+        success:true,
+        date:users[userIndex],
+        message:"User Updated"
+    })
+
+  } catch (err) {
+    return NextResponse.json({
+        success:false, 
+        message:"User Not Updated"
+    },{status:500})
+  }
 }
