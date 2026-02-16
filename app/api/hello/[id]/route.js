f5e24cb5f5e902d39a009cfe495995c03b84e5ee
Shivam <shivam108ku@gmail.com>
2026-02-16 13:06:33 +0530

    addign the 404

--- app/api/hello/[id]/route.js
+++ app/api/hello/[id]/route.js
@@ -1,23 +1,34 @@
 import { NextResponse } from "next/server";
 import { users } from "../route";
 
-export async function GET(request , {params}){
-    try {
+export async function GET(request, { params }) {
+  try {
+    const { id } = await params;
+    const userId = parseInt(id);
+    const user = users.find((u) => u.id === userId);
 
-        const {id} = await params 
-        const userId = parseInt(id)
-        const user = users.find(u=>u.id === userId)
-
-        return NextResponse.json({
-            success: true,
-            data: user,
-        });
-    } catch (error) {
-        return NextResponse.json({
-            success: false,
-            message: "An error occurred",
-            error: error?.message || error
-        }, { status: 500 });
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 },
+      );
     }
 
-} 
+    return NextResponse.json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "An error occurred",
+        error: error?.message || error,
+      },
+      { status: 500 },
+    );
+  }
+}
