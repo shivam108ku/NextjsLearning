f635154093536dfdec4e15134676905fe015e0e6
Shivam <shivam108ku@gmail.com>
2026-02-16 17:11:35 +0530

    Notes Schema

---
+++ models/Note.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const NoteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxLength: 2000,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+});
+
+NoteSchema.pre("save", function(next){
+    this.updatedAt = Date.now()
+    next();
+})
+
+export default mongoose.models.Note || mongoose.model("Note", NoteSchema);
