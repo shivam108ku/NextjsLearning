41325f91ac72062f4b1b4ac5d37ce2b4c151c479
Shivam <shivam108ku@gmail.com>
2026-02-16 13:04:27 +0530

    bug fixes

---
+++ app/api/hello/[id]/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { users } from "../route";
+
+export async function GET(request , {params}){
+    try {
+
+        const {id} = await params 
+        const userId = parseInt(id)
+        const user = users.find(u=>u.id === userId)
+
+        return NextResponse.json({
+            success: true,
+            data: user,
+        });
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: "An error occurred",
+            error: error?.message || error
+        }, { status: 500 });
+    }
+
+} 
