62bf2bb4540371690a95f61afb06790fafd82d78
Shivam <shivam108ku@gmail.com>
2026-02-18 11:22:29 +0530

    Implement getNotes function and update tsconfig to include JavaScript files

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
 import dbConnect from "@/lib/db";
 import NotesClient from "@/components/NotesClient"
-export default async function Home() {
+import Note from "@/models/Note"
 
+async function getNotes(){
   await dbConnect();
+  const notes = await Note.find({}).sort({createdAt:-1})
+  return notes.map({note}=>({
+    ...note,
+    _id: note._id.toString(),
+  }))
+}
+
+export default async function Home() {
   return (
      <div className="flex flex-col h-screen justify-center items-center">
           <h1 className="text-4xl text-green-500 font-extrabold">Notes App</h1>
